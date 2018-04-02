22132c8d5ec1f7f84f8116cd93b01e46a9fea79d
josephcuellar2662 <31263436+josephcuellar2662@users.noreply.github.com>
2018-04-02 14:20:42 -0400

    removed dao tests

--- TuitionReimbursementApp/src/Test/AppTest.java
+++ TuitionReimbursementApp/src/Test/AppTest.java
@@ -91,49 +91,49 @@ public class AppTest extends TestCase {
 	
 	/* ------------------------ TEST DAO --------------------------*/
 	//TEST EMPLOYEE DAO
-	public void testEmployeeDaoRetrieveByUsername(){
-		Employee employee = e_dao.retrieveEmployeeByUsername("benco");
-        System.out.println(employee);
-		assertEquals("Ben",employee.getFirstName());	
-	}
-	public void testEmployeeDaoRetrieveIdByUsername() {
-		int id = e_dao.retrieveIdByUsername("benco");
-		assertEquals(5, id);
-	}
+// 	public void testEmployeeDaoRetrieveByUsername(){
+// 		Employee employee = e_dao.retrieveEmployeeByUsername("benco");
+//         System.out.println(employee);
+// 		assertEquals("Ben",employee.getFirstName());	
+// 	}
+// 	public void testEmployeeDaoRetrieveIdByUsername() {
+// 		int id = e_dao.retrieveIdByUsername("benco");
+// 		assertEquals(5, id);
+// 	}
 
 	
 	//TEST EVENT DAO
-	public void testEventDaoCreate(){
-		Event event = new Event(1,"Training for aws certification");
-		assertTrue(event_dao.createEvent(event));
-	}
+// 	public void testEventDaoCreate(){
+// 		Event event = new Event(1,"Training for aws certification");
+// 		assertTrue(event_dao.createEvent(event));
+// 	}
 	
-	public void testEventDaoRetrieve(){
-		Event event = event_dao.retrieveEventById(1);
-		assertEquals("Training for aws certification", event.getDescription());
-	}
+// 	public void testEventDaoRetrieve(){
+// 		Event event = event_dao.retrieveEventById(1);
+// 		assertEquals("Training for aws certification", event.getDescription());
+// 	}
 	
 	//TEST REQUEST FORM DAO
-	public void testRequestFormDaoCreate() {
-		RequestForm reqForm = new RequestForm(9, "02-FEB-18", "11730 Plaza America Dr #205", "VA", "Reston", 20190, 800.0,
-				"this is work related", "letter grade","Certification", "approved", "approved", "approved");
-		assertEquals(true, rf_dao.createRequestForm(reqForm));
-		assertEquals(true, rf_dao.deleteRequestFormByEmployeeId(9));
-	}
-	public void testRequestFormDaoRetrieveAllApps() {
-		ArrayList<RequestForm> list = new ArrayList<>();
-		list = rf_dao.retrieveAllApplications();
-		for(int i=0; i< list.size(); i++) {
-			System.out.println(list.get(i));
-		}
-		assertEquals(1, 1);
-	}
-	public void testRequestFormDaoUpdateForm() {
-		assertEquals(true, rf_dao.updateForm(43,"approved"));
-	}
-	public void testRequestFormDaoRetrieveById() {
-//		RequestForm form = rf.retrieveRequestFormById(1);
-//		assertEquals("VA", form.getState());
-	}
+// 	public void testRequestFormDaoCreate() {
+// 		RequestForm reqForm = new RequestForm(9, "02-FEB-18", "11730 Plaza America Dr #205", "VA", "Reston", 20190, 800.0,
+// 				"this is work related", "letter grade","Certification", "approved", "approved", "approved");
+// 		assertEquals(true, rf_dao.createRequestForm(reqForm));
+// 		assertEquals(true, rf_dao.deleteRequestFormByEmployeeId(9));
+// 	}
+// 	public void testRequestFormDaoRetrieveAllApps() {
+// 		ArrayList<RequestForm> list = new ArrayList<>();
+// 		list = rf_dao.retrieveAllApplications();
+// 		for(int i=0; i< list.size(); i++) {
+// 			System.out.println(list.get(i));
+// 		}
+// 		assertEquals(1, 1);
+// 	}
+// 	public void testRequestFormDaoUpdateForm() {
+// 		assertEquals(true, rf_dao.updateForm(43,"approved"));
+// 	}
+// 	public void testRequestFormDaoRetrieveById() {
+// 		RequestForm form = rf.retrieveRequestFormById(1);
+// 		assertEquals("VA", form.getState());
+// 	}
 	
 }
